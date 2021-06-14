8554e70b644355723d4da3448fdf541feff4f258
Julius Jurgelenas <julius@jurgelenas.lt>
2021-06-14 09:25:24 +0300

    Do not return old git tags to the users

--- src/api/src/services/GitHubClient/index.ts
+++ src/api/src/services/GitHubClient/index.ts
@@ -21,7 +21,20 @@ export default class OctopusGitHubClient implements IGitHubClient {
       repo: repository,
       per_page: 50,
     });
-    return response.data.map((item) => item.name);
+    const blacklist = [
+      '0.1.0-RC1',
+      '0.1.0-RC2',
+      '0.1.3-RC2',
+      '1.0.0-RC1',
+      '1.0.0-RC2',
+      '1.0.0-RC3',
+      '1.0.0-RC5',
+      '1.0.0-RC6',
+      '1.0.0-RC7',
+    ];
+    return response.data
+      .map((item) => item.name)
+      .filter((name) => !blacklist.includes(name));
   }
 
   async loadBranches(owner: string, repository: string): Promise<string[]> {
