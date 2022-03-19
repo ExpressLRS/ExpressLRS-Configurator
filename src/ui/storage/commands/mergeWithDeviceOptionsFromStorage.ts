22659d0c1a2e5448c839371aa82ee56e61f15b13
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2022-03-19 11:58:33 -0700

    Revert "Add: documentation for mergeWithDeviceOptionsFromStorage.ts"
    This reverts commit abe1f81c7f1e3b79696367239c898c0199df37e6.

--- src/ui/storage/commands/mergeWithDeviceOptionsFromStorage.ts
+++ src/ui/storage/commands/mergeWithDeviceOptionsFromStorage.ts
@@ -1,7 +1,3 @@
-/* Load user options from storage and update DeviceOptions object
- *  to reflect what is stored.
- */
-
 import {
   UserDefineOptionGroup,
   UserDefine,
@@ -14,7 +10,6 @@ const mergeWithDeviceOptionsFromStorage = async (
   device: string | null,
   deviceOptions: DeviceOptions
 ): Promise<DeviceOptions> => {
-  // Load saved user options from storage
   const savedBindingPhrase = await storage.getBindingPhrase();
   const savedTargetOptions = device
     ? await storage.getDeviceOptions(device)
@@ -24,11 +19,6 @@ const mergeWithDeviceOptionsFromStorage = async (
   const regulatoryDomain900 = await storage.getRegulatoryDomain900();
   const regulatoryDomain2400 = await storage.getRegulatoryDomain2400();
 
-  /*
-   * Updates the value of a given device option to a storage value where possible
-   * @Param {UserDefine} deviceOption - a single user defined device option
-   * @Returns {UserDefine} deviceOtion with updated value
-   */
   const addOverrides = (deviceOption: UserDefine): UserDefine => {
     if (
       deviceOption.key === UserDefineKey.BINDING_PHRASE &&
@@ -75,12 +65,6 @@ const mergeWithDeviceOptionsFromStorage = async (
     }
     return deviceOption;
   };
-
-  /*
-   * Replaces values in an array of user options to stored values
-   * @Param {UserDefine[]} array of user options
-   * @Returns {UserDefine[]} array of user options with values replaced
-   */
   const userDefineOptions = deviceOptions.userDefineOptions.map(
     (deviceOption) => {
       if (savedTargetOptions === null) {
